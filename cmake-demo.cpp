68e55f94fd2ea1e3fc183a4daf6206e548838ffc
Corentin Rondier <corentin.rondier@epitech.eu>
2023-05-28 18:10:37 +0200

    Ajoutez des fichiers projet.

---
+++ cmake-demo.cpp
@@ -0,0 +1,12 @@
+﻿// cmake-demo.cpp : définit le point d'entrée de l'application.
+//
+
+#include "cmake-demo.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
